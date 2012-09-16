48f8cdff22253c107413d916b263489231f82cf1
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-16 21:56:16 +0300

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -35,4 +36,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Mix-in FG's methods so you don't have to call FactoryGirl in the tests
+  # http://robots.thoughtbot.com/post/19162390206/short-explicit-test-setups
+  config.include FactoryGirl::Syntax::Methods
 end
