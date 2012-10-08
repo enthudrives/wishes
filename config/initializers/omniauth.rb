474ce218a461d66fce9725d60dda242caa6428f7
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-10-09 01:10:40 +0300

    Add Github authorization keys for both production & development environments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '21a4e53be1df1fb8873a', '544b1dc2eea4376e17a19604da0a336cf9af2e73'
+  provider :github, '21a4e53be1df1fb8873a', '544b1dc2eea4376e17a19604da0a336cf9af2e73' if Rails.env.development?
+  provider :github, '4910999bb952e346cd54', '11e4cd12861a3cb169b3cff584f0542fb70fa307' if Rails.env.production?
 end
